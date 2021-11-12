7476984b5483489714e74e2a0cdfb877eec2dd7f
Safeer Ahmed <sahme73@illinois.edu>
2021-11-11 22:38:06 -0600

    Created a folder for Unit Testing. Within the folder there are two types of testing, PlayMode and EditMode. I added assembly definitions to the Scripts folder to connect the scripts to the Unit Testing assemblies. Within the Unit Test scripts, I made templates to test our movement scripts (still need further edits).

---
+++ unity-folder/project/Assets/Tests/EditMode/MovementTest.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class MovementTest
+{
+  // A Test behaves as an ordinary method
+  [Test]
+  public void UpwardChange() {
+    // Use the Assert class to test conditions
+    Assert.AreEqual(new Vector2(0, 0), new Vector2(0, 0));
+  }
+
+  // A Test behaves as an ordinary method
+  [Test]
+  public void DownwardChange() {
+    // Use the Assert class to test conditions
+    Assert.AreEqual(new Vector2(0, 0), new Vector2(0, 0));
+  }
+
+  // A Test behaves as an ordinary method
+  [Test]
+  public void RightwardChange() {
+    // Use the Assert class to test conditions
+    Assert.AreEqual(new Vector2(0, 0), new Vector2(0, 0));
+  }
+
+  // A Test behaves as an ordinary method
+  [Test]
+  public void LeftwardChange() {
+    // Use the Assert class to test conditions
+    Assert.AreEqual(new Vector2(0, 0), new Vector2(0, 0));
+  }
+}
