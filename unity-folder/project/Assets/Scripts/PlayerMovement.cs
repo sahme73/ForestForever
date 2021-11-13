0b8ad94caf4b17b8e681fd1eba545cc83da52622
Safeer Ahmed <sahme73@illinois.edu>
2021-11-12 19:31:56 -0600

    Movement Unit Test Update
    Another update to the movement script and finished adapting the movement unit testing for EditMode testing.

--- unity-folder/project/Assets/Scripts/PlayerMovement.cs
+++ unity-folder/project/Assets/Scripts/PlayerMovement.cs
@@ -2,35 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TopDownMovement {
   /*This script is a basic implementation of movement in a 2D space.*/
 
 public class PlayerMovement : MonoBehaviour {
-    public Rigidbody2D player; // rigidbody for the current player object
-    public float speed = 10.0f; // the adjustable speed of the player
+  public Rigidbody2D player; // rigidbody for the current player object
+  public float speed = 10.0f; // the adjustable speed of the player
 
-    private Vector2 movement; // the 2d vector of movement speeds
+  private Vector2 movement; // the 2d vector of movement velocity (includes direction)
 
-    // Start is called before the first frame update
-    private void Start() {
-      player = GetComponent<Rigidbody2D>(); // initializes the player's rigidbody to the current object
-    }
-
-    // Update is called once per frame | Input handler
-    private void Update() {
-      // Updated Approach //
-      movement.x = Input.GetAxisRaw("Horizontal");
-      movement.y = Input.GetAxisRaw("Vertical");
-    }
-
-    // Update is called 50 times per second | Physics handler
-    private void FixedUpdate() {
-      Move();
-    }
-
-    public void Move() {
-      player.MovePosition(player.position + movement * speed * Time.fixedDeltaTime);
-    }
+  // Start is called before the first frame update
+  private void Start() {
+    player = GetComponent<Rigidbody2D>(); // initializes the player's rigidbody to the current object
   }
 
+  // Update is called once per frame | Input handler
+  private void Update() {
+    // Updated Approach //
+    movement.x = Input.GetAxisRaw("Horizontal");
+    movement.y = Input.GetAxisRaw("Vertical");
+  }
+
+  // Update is called 50 times per second | Physics handler
+  private void FixedUpdate() {
+    Move();
+  }
+
+  public void Move() {
+    player.MovePosition(MoveCalculation());
+  }
+
+  public Vector2 MoveCalculation() {
+    return (player.position + movement * speed * Time.fixedDeltaTime);
+  }
+
+  public void SetMovement(float x, float y) {
+    movement.x = x;
+    movement.y = y;
+  }
 }
