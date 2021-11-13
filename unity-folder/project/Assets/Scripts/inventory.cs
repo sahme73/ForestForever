0aaaaca680cc1bd104932714e2636d2630da107a
Daniel Feng <dwfeng2@illinois.edu>
2021-11-13 14:58:48 -0600

    Setting up an inventory file

---
+++ unity-folder/project/Assets/Scripts/inventory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Daniel Test");
+    }
+}
