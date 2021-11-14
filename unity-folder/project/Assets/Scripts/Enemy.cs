588bfef6af50ce9fa791124547e7566de5bb0dcc
Carter Smith <carters3@illinois.edu>
2021-11-14 06:24:58 +0000

    Added ability to track player health and modify

--- unity-folder/project/Assets/Scripts/Enemy.cs
+++ unity-folder/project/Assets/Scripts/Enemy.cs
@@ -7,6 +7,10 @@ public class Enemy : MonoBehaviour
     public float speed = 3f;
     private Transform target;
 
+    [SerializeField] private float basicDamage = 10f;
+    [SerializeField] private float basicSpeed = 1f;
+    private float canAttack;
+
     private void FixedUpdate() {
         if (target) {
             float step = speed * Time.deltaTime;
@@ -17,13 +21,24 @@ public class Enemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
             target = other.transform;
-
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
             target = null;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            if (basicSpeed <= canAttack) {
+                other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-basicDamage);
+                canAttack = 0f;
+            } else {
+                canAttack += Time.deltaTime;
+            }
+            
         }
     }
 }
