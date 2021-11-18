6e5aca52e40a6d3b728b98fdfad7d49b683ce7de
Ben Evanoff <55565574+benevanoff@users.noreply.github.com>
2021-11-18 10:52:10 -0600

    basic inventory UI

--- unity-folder/project/Assets/Scripts/PlayerActions.cs
+++ unity-folder/project/Assets/Scripts/PlayerActions.cs
@@ -7,6 +7,8 @@ public class PlayerActions : MonoBehaviour
     public Collider2D player;
     public Collider2D tree;
 
+    private bool showInventory = false;
+
     //public PlayerInventory inventory;
 
     private void Update() {
@@ -17,8 +19,21 @@ public class PlayerActions : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown("e")) {
-            Debug.Log(player.GetComponent<PlayerInventory>().ToString());
+        if (Input.GetKeyDown("e")) showInventory = !showInventory; // toggle inventory shown/hidden by clicking 'e'
+    }
+
+    void OnGUI() {
+        if (showInventory) {
+            PlayerInventory inventory = player.GetComponent<PlayerInventory>();
+            Dictionary<string, int> items = inventory.GetItems();
+            //Debug.Log("Drawing inventory");
+            float yVal = (Screen.height / 2) - 150;
+            GUI.Box(new Rect((Screen.width / 2) - 35, yVal, 75, 20 + (30*items.Count)), "Inventory");
+            foreach(var item in items) {
+                yVal += 30;
+                Debug.Log("Drawing " + item.Key + " : " + item.Value);
+                GUI.Label(new Rect((Screen.width / 2) - (item.Key.Length  * 6), yVal, 100, 20), item.Key + " : " + item.Value.ToString());
+            }
         }
     }
 }
