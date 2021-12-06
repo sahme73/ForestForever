6b1b0597718839582ed633b0a0230eba048bba6b
Daniel Feng <dwfeng2@illinois.edu>
2021-12-06 14:03:13 -0600

    Adding more inventory tests and fixes.

--- unity-folder/project/Assets/Tests/EditMode/InventoryTest.cs
+++ unity-folder/project/Assets/Tests/EditMode/InventoryTest.cs
@@ -11,15 +11,15 @@ public class Health
         GameObject temp = new GameObject("temp");
         temp.AddComponent<PlayerInventory>();
 
-        GetComponent<PlayerInventory>().AddItem("Wood", 10);
-        GetComponent<PlayerInventory>().AddItem("Seed", 100);
+        temp.GetComponent<PlayerInventory>().AddItem("Wood", 10);
+        temp.GetComponent<PlayerInventory>().AddItem("Seed", 100);
 
         Assert.AreEqual(
-            GetComponent<PlayerInventory>().GetCount("Wood"), 
+            temp.GetComponent<PlayerInventory>().GetCount("Wood"), 
             10);
 
         Assert.AreEqual(
-            GetComponent<PlayerInventory>().GetCount("Seed"), 
+            temp.GetComponent<PlayerInventory>().GetCount("Seed"), 
             100);
 
         // destroy dummy object:
@@ -31,22 +31,22 @@ public class Health
         GameObject temp = new GameObject("temp");
         temp.AddComponent<PlayerInventory>();
 
-        GetComponent<PlayerInventory>().AddItem("Wood", 10);
-        GetComponent<PlayerInventory>().AddItem("Tinder", 10);
+        temp.GetComponent<PlayerInventory>().AddItem("Wood", 10);
+        temp.GetComponent<PlayerInventory>().AddItem("Tinder", 10);
 
         Assert.IsTrue(GetComponent<PlayerInventory>().Craft("Small Campfire"));
 
 
         Assert.AreEqual(
-            GetComponent<PlayerInventory>().GetCount("Wood"), 
+            temp.GetComponent<PlayerInventory>().GetCount("Wood"), 
             0);
 
         Assert.AreEqual(
-            GetComponent<PlayerInventory>().GetCount("Tinder"), 
+            temp.GetComponent<PlayerInventory>().GetCount("Tinder"), 
             0);
 
         Assert.AreEqual(
-            GetComponent<PlayerInventory>().GetCount("Small Campfire"), 
+            temp.GetComponent<PlayerInventory>().GetCount("Small Campfire"), 
             1);
 
         // destroy dummy object:
@@ -58,22 +58,68 @@ public class Health
         GameObject temp = new GameObject("temp");
         temp.AddComponent<PlayerInventory>();
 
-        GetComponent<PlayerInventory>().AddItem("Wood", 9);
-        GetComponent<PlayerInventory>().AddItem("Tinder", 10);
+        temp.GetComponent<PlayerInventory>().AddItem("Wood", 9);
+        temp.GetComponent<PlayerInventory>().AddItem("Tinder", 10);
 
-        Assert.IsTrue(!GetComponent<PlayerInventory>().Craft("Small Campfire"));
+        Assert.IsTrue(!temp.GetComponent<PlayerInventory>().Craft("Small Campfire"));
 
         Assert.AreEqual(
-            GetComponent<PlayerInventory>().GetCount("Wood"), 
+            temp.GetComponent<PlayerInventory>().GetCount("Wood"), 
             9);
 
         Assert.AreEqual(
-            GetComponent<PlayerInventory>().GetCount("Tinder"), 
+            temp.GetComponent<PlayerInventory>().GetCount("Tinder"), 
             10);
 
         Assert.AreEqual(
-            GetComponent<PlayerInventory>().GetCount("Small Campfire"), 
+            temp.GetComponent<PlayerInventory>().GetCount("Small Campfire"), 
             0);
+
+        // destroy dummy object:
+        GameObject.DestroyImmediate(temp);
+    }
+
+    [Test]
+    public void AddCraft() {
+        GameObject temp = new GameObject("temp");
+        temp.AddComponent<PlayerInventory>();
+
+        temp.GetComponent<PlayerInventory>().AddItem("A", 5);
+        temp.GetComponent<PlayerInventory>().AddItem("B", 5);
+
+        temp.GetComponent<PlayerInventory>().AddCrafting(
+            "C", 
+            new Dictionary<string, int>(){
+                {"A", 5},
+                {"B", 5}
+            });
+
+        Assert.IsTrue(temp.GetComponent<PlayerInventory>().Craft("C"));
+
+
+        // destroy dummy object:
+        GameObject.DestroyImmediate(temp);
+    }
+
+    [Test]
+    public void SaveHash() {
+        GameObject temp = new GameObject("temp");
+        temp.AddComponent<PlayerInventory>();
+
+        temp.GetComponent<PlayerInventory>().AddItem("ABCDE", 5);
+        temp.GetComponent<PlayerInventory>().AddItem("BXXXX", 5);
+
+        GameObject temp2 = new GameObject("temp");
+        temp2.AddComponent<PlayerInventory>();
+
+        temp2.GetComponent<PlayerInventory>().AddItem("ABCDE", 5);
+        temp2.GetComponent<PlayerInventory>().AddItem("BXXXX", 5);
+
+        Assert.AreEqual(
+            temp1.GetComponent<PlayerInventory>().ToSaveHash(),
+            temp2.GetComponent<PlayerInventory>().ToSaveHash(),
+        );
+
 
         // destroy dummy object:
         GameObject.DestroyImmediate(temp);
